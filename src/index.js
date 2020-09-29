b73e0e8a16ad560378c0528416c30d9e14175e2e
Albinous <68611345+Albinous@users.noreply.github.com>
2020-09-29 20:16:52 +0500

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0
+  let average;
+  if (array === undefined || array.length === 0) {
+    return 0
+  } else {
+    for(let i=0; i<array.length; i++) {
+      sum+=array[i]
+    }
+    average = sum/array.length
+    return average;
+  }
 }
